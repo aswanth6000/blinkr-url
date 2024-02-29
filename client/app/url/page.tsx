56816f328870b71244e6ca2b56580ccc4a978066
aswanth6000 <aswanth682@gmail.com>
2024-02-29 21:37:59 +0530

    clerk done

---
+++ client/app/url/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='h-full w-full bg-purple-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 border border-gray-100
+    "'>page</div>
+  )
+}
+
+export default page
---
+++ client/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav className='shadow-md h-48 w-full'>navbar</nav>
+  )
+}
+
+export default navbar
