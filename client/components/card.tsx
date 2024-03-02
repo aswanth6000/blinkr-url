4cc5a7d126ef9754fae896871140d5d91f81d00b
aswanth6000 <aswanth682@gmail.com>
2024-03-02 11:06:01 +0530

    client side validation done

--- client/components/card.tsx
+++ client/components/card.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
@@ -8,27 +8,28 @@ import { useUser } from "@clerk/clerk-react";
 import { toast } from "sonner"
 
 
+
 const Card = () => {
   const [link, setLink] = useState("");
   const [isValid, setIsValid] = useState(true);
-  const { isSignedIn, user, isLoaded } = useUser();
+  const { user } = useUser();
 
+  useEffect(() => {
+    const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+    setIsValid(urlRegex.test(link));
+  }, [link]);
 
-  const sendUserInput = (data: {userId: string | undefined, url: string}) =>{
-   
-    
+  const sendUserInput = (data: {userId: string | undefined, url: string}) => {
     axios.post('/url', data, {
       headers: {
         "Content-Type": 'application/json'
       }
     })
-    .then((response)=>{
+    .then((response) => {
       console.log(response, "success response");
-      
     })
-    .catch((error)=>{
+    .catch((error) => {
       console.log("error", error);
-      
       toast.error(error.message)
     })
   }
@@ -36,28 +37,26 @@ const Card = () => {
   const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setLink(e.target.value);
   };
+
   const handleSubmit = (e: { preventDefault: () => void; }) => {
-    console.log(user);
     e.preventDefault();
     const data = {
       userId: user?.id,
       url: link
-    }
+    };
     console.log("Data to send:", data);
-    console.log(isValid);
     
-
-      sendUserInput(data)
-
+    if (isValid) {
+      sendUserInput(data);
+    }
   };
-
 
   return (
     <form onSubmit={handleSubmit} className="h-96 flex justify-center w-8/12 bg-purple-50 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100">
         <div className="flex">
       <div className="flex w-full max-w-sm items-center space-x-2 mb-4">
         <Input value={link} onChange={handleInputChange}  type="text" placeholder="Enter the link" />
-        <Button type="submit">Short link</Button>
+        <Button disabled={!isValid} type="submit">Short link</Button>
       </div>
         </div>
     </form>
