f786b0f2002be9de41d28077e888860624df569d
aswanth6000 <aswanth682@gmail.com>
2024-03-05 12:27:44 +0530

    build

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/framework/index.ts"],
   format: ["cjs", "esm"], 
   dts: true, 
   splitting: false,
